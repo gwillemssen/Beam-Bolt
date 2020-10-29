62752561eb47dc34003465787c74777ee084fff1
gwillemssen <57201850+gwillemssen@users.noreply.github.com>
2020-10-29 16:43:57 -0500

    added comments to code

--- Assets/Scripts/BeamManager.cs
+++ Assets/Scripts/BeamManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BeamManager : MonoBehaviour
 {
+    //amount of beams spawning each time
     private enum difficultyState
     {
         easy, medium, hard, expert
@@ -19,6 +20,7 @@ public class BeamManager : MonoBehaviour
     private float spawnHeight = -102;
     private float lastSpawnTime;
 
+    //time at which difficulty will switch
     [SerializeField]
     private float mediumTime = 20f, hardTime = 50f, expertTime = 75f;
     private difficultyState state;
@@ -29,6 +31,7 @@ public class BeamManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //begins in easy with the speed at default and spawn time at default
         timer = 0;
         lastSpawnTime = 0;
         spawnTime = baseSpawnTime;
@@ -39,34 +42,41 @@ public class BeamManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if the speed is less than the max speed, increase every frame
         timer += Time.deltaTime;
         if (BeamMovement.speed < BeamMovement.maxSpeed)
         {
             BeamMovement.speed += Time.deltaTime / 2;
         }
+        //increasing the spawn rate every frame as well
         spawnTime = baseSpawnTime / (BeamMovement.speed / startingSpeed);
 
+        //spawn layer of beams
         if (timer >= lastSpawnTime + spawnTime)
         {
             lastSpawnTime = timer;
             SpawnRandomBeamLayer();
         }
 
+        //switch to meduim difficuty
         if (timer >= mediumTime && timer < hardTime)
         {
             Debug.Log("Medium");
             state = difficultyState.medium;
         }
+        //switch to hard difficulty
         else if (timer >= hardTime && timer < expertTime)
         {
             Debug.Log("hard");
             state = difficultyState.hard;
         }
+        //switch to expert difficulty
         else if (timer >= expertTime)
         {
             Debug.Log("expert");
             state = difficultyState.expert;
         }
+        //stay at easy difficulty in beginning
         else
         {
             Debug.Log("easy");
@@ -76,6 +86,7 @@ public class BeamManager : MonoBehaviour
         
     }
 
+    //picking randomly either a vertical or horizontal beam to spawn
     private GameObject PickBeam()
     {
         int randomNumber = Random.Range(0, 2);
@@ -90,6 +101,7 @@ public class BeamManager : MonoBehaviour
         return null; //never should hit anything other than 1 or 0
     }
 
+    //randomly pick a point to spawn each beam in the game space
     private Vector3 SpawnPoint(GameObject beam)
     {
         if (beam.transform.rotation.eulerAngles.y == 90f) //vertical 5-19
@@ -108,12 +120,14 @@ public class BeamManager : MonoBehaviour
         return beam.transform.position; //never should hit anything other than 0 or 90
     }
 
+    //actually spawn the beams using the pickbeam and spawnpoint functions
     private void SpawnRandomBeam()
     {
         GameObject beam = PickBeam();
         Instantiate(beam, SpawnPoint(beam), beam.transform.rotation);
     }
 
+    //how many beams spawn at a time in each difficulty
     private void SpawnRandomBeamLayer()
     { 
         switch (state)
